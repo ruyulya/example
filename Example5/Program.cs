58f3be1f38bd3b6bb03c689bc0ff8b92c33b3f49
ruyulya <237451687@mail.ru>
2022-07-04 23:07:35 +0300

    Add ex5 hey username

---
+++ Example5/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Add user name");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "yulia")
+{
+    Console.WriteLine("Wow, it's Yulia!");
+}
+else
+{
+    Console.Write("Hey, ");
+    Console.WriteLine(username);
+}
